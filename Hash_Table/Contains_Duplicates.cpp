6184cc18cf7bd36116cfa5f2866ed0015dd72789
octopus-me <guiwallace96@gmail.com>
2025-02-15 09:07:10 -0300

    hashSet: contains duplicates

---
+++ Hash_Table/Contains_Duplicates.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set <int> hashset;
+
+        for(int i: nums){
+            if(hashset.count(i) > 0){
+                return true;
+            }
+            hashset.insert(i);
+        }
+        return false;
+        
+    }
+};
